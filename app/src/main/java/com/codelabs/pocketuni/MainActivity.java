5ab1eddf35c1f6a850ead49ca85d078aa5c676b0
shehan-shyaminda <shehan.shyaminda@gmail.com>
2022-05-23 22:39:24 +0530

    initial commit

---
+++ app/src/main/java/com/codelabs/pocketuni/MainActivity.java
@@ -0,0 +1,147 @@
+package com.codelabs.pocketuni;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.applandeo.materialcalendarview.CalendarView;
+import com.codelabs.pocketuni.ui.AnnouncementsFragment;
+import com.codelabs.pocketuni.ui.HomeFragment;
+import com.codelabs.pocketuni.ui.NoticesFragment;
+import com.codelabs.pocketuni.ui.ProfileFragment;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.navigation.NavigationView;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private NavigationView nvDrawer;
+    private AnimatedBottomBar bottomBar;
+    private ImageView icMenu, btnCalender;
+    private TextView fragmentTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomBar = findViewById(R.id.bottomMenu);
+        fragmentTitle = findViewById(R.id.fragmentTitle);
+        icMenu = findViewById(R.id.ic_menu);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
+        bottomBar.selectTabById(R.id.home,true);
+        fragmentTitle.setText("My Events");
+
+        SideDrawerInitializing();
+
+        icMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDrawerPosition();
+            }
+        });
+
+        bottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int i, @Nullable AnimatedBottomBar.Tab tab, int i1, @NonNull AnimatedBottomBar.Tab tab1) {
+                Fragment fragment = null;
+                String title = "";
+                switch (tab1.getId()) {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        title = "My Events";
+                        break;
+                    case R.id.notices:
+                        fragment = new NoticesFragment();
+                        title = "Batch Notices";
+                        break;
+                    case R.id.announcements:
+                        fragment = new AnnouncementsFragment();
+                        title = "Public Notices";
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        title = "My Profile";
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
+                fragmentTitle.setText(title);
+            }
+
+            @Override
+            public void onTabReselected(int i, @NonNull AnimatedBottomBar.Tab tab) {
+
+            }
+        });
+    }
+
+    //-----------side drawer implementation---------------
+    private void SideDrawerInitializing() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        nvDrawer = (NavigationView) findViewById(R.id.nav_view);
+        mToggle.syncState();
+        setupDrawerContent(nvDrawer);
+//        ic_menuOpen = nvDrawer.findViewById(R.id.nav_drawer_open);
+//
+//        ic_menuOpen.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                setDrawerPosition();
+//            }
+//        });
+
+        //----------------pass value to header---------------
+        View hView = nvDrawer.getHeaderView(0);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.contactUs:
+                        Log.e(TAG, "onNavigationItemSelected: " + item.getTitle());
+                        break;
+                    case R.id.aboutUs:
+                        Log.e(TAG, "onNavigationItemSelected: " + item.getTitle());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void setDrawerPosition() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            mDrawerLayout.setVisibility(View.VISIBLE);
+        } else {
+            mDrawerLayout.openDrawer(Gravity.LEFT);
+        }
+    }
+
+    public void sideDrawerOpen(View v) {
+        setDrawerPosition();
+    }
+}
---
+++ app/src/main/java/com/codelabs/pocketuni/ui/HomeFragment.java
@@ -0,0 +1,42 @@
+package com.codelabs.pocketuni.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.applandeo.materialcalendarview.CalendarView;
+import com.codelabs.pocketuni.MainActivity;
+import com.codelabs.pocketuni.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class HomeFragment extends Fragment {
+
+    private ImageView btnCalender;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btnCalender = view.findViewById(R.id.btn_calender);
+
+        btnCalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext(), R.style.BottSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getContext()).inflate(R.layout.bottomsheet_calender, view.findViewById(R.id.bottom_sheet_container));
+                CalendarView calendarView = bottomSheetView.findViewById(R.id.calendarView);
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
+
+        return view;
+    }
+
+}
